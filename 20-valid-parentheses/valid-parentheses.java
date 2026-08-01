9de18620588fec1e294916c93143dc50d41af043
Nikshep D <nikshepd01@gmail.com>
2026-08-02 01:41:25 +0530

    Time: 4 ms (36.82%) | Memory: 43.1 MB (86.21%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }
+            else{
+                if(!stack.isEmpty()){
+                    char top = stack.pop();
+                    if ((top == '(' && ch == ')') || (top == '{' && ch == '}') || (top == '[' && ch == ']')){
+                        continue;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        } 
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
