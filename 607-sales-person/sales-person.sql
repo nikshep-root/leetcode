f8225866da7f3aa16f351cb59bcf6d8a25f324fd
Nikshep D <nikshepd01@gmail.com>
2026-08-03 00:15:11 +0530

    Time: 323 ms (78.05%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
 
-select name from SalesPerson where sales_id not in (select sales_id from Orders where com_id = (select com_id from Company where name = 'RED'))
+select name from SalesPerson where sales_id not in (select sales_id from Orders where com_id IN (select com_id from Company where name = 'RED'))
