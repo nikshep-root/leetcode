d8ce6b8a23caca832043f3a8b5250ba299630cf4
Nikshep D <nikshepd01@gmail.com>
2026-09-09 20:35:36 +0530

    Time: 1 ms (99.36%) | Memory: 42.7 MB (56.41%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long count = 0;
+        if (n >= 1000L)count += n - 999L; 
+        if (n >= 1000000L)count += n - 999999L; 
+        if (n >= 1000000000L)count += n - 999999999L; 
+        if (n >= 1000000000000L) count += n - 999999999999L; 
+        if (n >= 1000000000000000L)count += n - 999999999999999L; 
+        return count;
+    }
+}
